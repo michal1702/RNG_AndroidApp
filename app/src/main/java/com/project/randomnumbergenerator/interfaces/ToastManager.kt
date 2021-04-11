baf0d94181e6e2e8b61a3ba7883aa029f118214d
michal1702 <michalopielka17@gmail.com>
2021-04-11 12:51:55 +0200

    added onViewCreated

--- app/src/main/java/com/project/randomnumbergenerator/interfaces/ToastManager.kt
+++ app/src/main/java/com/project/randomnumbergenerator/interfaces/ToastManager.kt
@@ -6,18 +6,10 @@ import android.widget.Toast
 interface ToastManager {
     val context: Context
 
-    /**
-     * Shows quick toast
-     * @param message toast message
-     */
     fun shortToast(message: String){
         Toast.makeText(this.context, message,Toast.LENGTH_SHORT).show()
     }
 
-    /**
-     * Shows long toast
-     * @param message toast message
-     */
     fun longToast(message: String){
         Toast.makeText(this.context, message,Toast.LENGTH_LONG).show()
     }
