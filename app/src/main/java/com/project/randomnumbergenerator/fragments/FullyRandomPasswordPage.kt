6882e746074ee825723824f518f647d5f840753c
michal1702 <michalopielka17@gmail.com>
2021-04-18 14:30:43 +0200

    introduced view binding inside fragments

--- app/src/main/java/com/project/randomnumbergenerator/fragments/FullyRandomPasswordPage.kt
+++ app/src/main/java/com/project/randomnumbergenerator/fragments/FullyRandomPasswordPage.kt
@@ -11,33 +11,36 @@ import android.widget.Button
 import android.widget.Spinner
 import android.widget.TextView
 import com.project.randomnumbergenerator.R
+import com.project.randomnumbergenerator.databinding.FragmentFullyRandomPasswordPageBinding
 import com.project.randomnumbergenerator.model.FullyRandomPasswordGenerator
 
 
 class FullyRandomPasswordPage : Fragment() {
+
+    private lateinit var binding: FragmentFullyRandomPasswordPageBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fully_random_password_page, container, false)
+        binding = FragmentFullyRandomPasswordPageBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val generateButton = view.findViewById<Button>(R.id.generateFullyRandomPasswordButton)
-        val generatedPasswordTextView = view.findViewById<TextView>(R.id.fullyRandomPasswordTextView)
-        val spinner = view.findViewById<Spinner>(R.id.passwordCharactersCountSpinner)
+
         val spinnerAdapter = ArrayAdapter.createFromResource(context!!,
                 R.array.passwordCharactersCount,
                 R.layout.spinner_layout
         )
         spinnerAdapter.setDropDownViewResource(R.layout.spinner_dropdown_layout)
-        spinner.adapter = spinnerAdapter
+        binding.passwordCharactersCountSpinner.adapter = spinnerAdapter
 
-        generateButton.setOnClickListener {
-            val generator = FullyRandomPasswordGenerator(spinner.selectedItem.toString().toInt())
-            generatedPasswordTextView.text = generator.getGeneratedPassword()
+        binding.generateFullyRandomPasswordButton.setOnClickListener {
+            val generator = FullyRandomPasswordGenerator(binding.passwordCharactersCountSpinner.selectedItem.toString().toInt())
+            binding.fullyRandomPasswordTextView.text = generator.getGeneratedPassword()
         }
     }
 
--- app/src/main/java/com/project/randomnumbergenerator/fragments/UserFriendlyPasswordPage.kt
+++ app/src/main/java/com/project/randomnumbergenerator/fragments/UserFriendlyPasswordPage.kt
@@ -10,6 +10,7 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.project.randomnumbergenerator.R
+import com.project.randomnumbergenerator.databinding.FragmentUserFriendlyPasswordPageBinding
 import com.project.randomnumbergenerator.interfaces.ToastManager
 import com.project.randomnumbergenerator.model.UserFriendlyPasswordGenerator
 
@@ -20,35 +21,31 @@ import com.project.randomnumbergenerator.model.UserFriendlyPasswordGenerator
 class UserFriendlyPasswordPage(context: Context) : Fragment(), ToastManager{
 
     override val activityContext: Context = context
+    private lateinit var binding: FragmentUserFriendlyPasswordPageBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_friendly_password_page, container, false)
+        binding = FragmentUserFriendlyPasswordPageBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     @ExperimentalStdlibApi
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val word1 = view.findViewById<EditText>(R.id.userWord1TextEdit)
-        val word2 = view.findViewById<EditText>(R.id.userWord2TextEdit)
-        val word3 = view.findViewById<EditText>(R.id.userWord3TextEdit)
-        val generatePassword = view.findViewById<Button>(R.id.generateUserPasswordButton)
-        val generatedPassword = view.findViewById<TextView>(R.id.generatedUserPasswordTextView)
         val wordsArray = Array(3) {""}
-        generatePassword.setOnClickListener {
+        binding.generateUserPasswordButton.setOnClickListener {
             var words: Int = 0
-            if(!word1.text.isNullOrEmpty()) {
-                wordsArray[0] = word1.text.toString()
+            if(!binding.userWord1TextEdit.text.isNullOrEmpty()) {
+                wordsArray[0] = binding.userWord1TextEdit.text.toString()
                 words++
             }
-            if(!word2.text.isNullOrEmpty()) {
-                wordsArray[1] = word2.text.toString()
+            if(!binding.userWord2TextEdit.text.isNullOrEmpty()) {
+                wordsArray[1] = binding.userWord2TextEdit.text.toString()
                 words++
             }
-            if(!word3.text.isNullOrEmpty()) {
-                wordsArray[2] = word3.text.toString()
+            if(!binding.userWord3TextEdit.text.isNullOrEmpty()) {
+                wordsArray[2] = binding.userWord3TextEdit.text.toString()
                 words++
             }
 
@@ -56,7 +53,7 @@ class UserFriendlyPasswordPage(context: Context) : Fragment(), ToastManager{
                 longToast("Enter at least 2 words")
             }else {
                 val passwordGenerator = UserFriendlyPasswordGenerator(wordsArray)
-                generatedPassword.text = passwordGenerator.getGeneratedPassword()
+                binding.generatedUserPasswordTextView.text = passwordGenerator.getGeneratedPassword()
             }
         }
     }
