39224e104b621eaf4284ebd2a920f4380728df8b
michal1702 <michalopielka17@gmail.com>
2021-04-21 14:00:37 +0200

    fixed sounds adapter

--- app/src/main/java/com/project/randomnumbergenerator/adapters/SoundsListAdapter.kt
+++ app/src/main/java/com/project/randomnumbergenerator/adapters/SoundsListAdapter.kt
@@ -2,18 +2,20 @@ package com.project.randomnumbergenerator.adapters
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
 import com.project.randomnumbergenerator.R
+import com.project.randomnumbergenerator.databinding.RandomSoundsListViewItemBinding
 import com.project.randomnumbergenerator.model.SoundManager
 import com.project.randomnumbergenerator.listitems.SoundsListItem
 
 class SoundsListAdapter(var context: Context, var arraySoundsItems: ArrayList<SoundsListItem>):BaseAdapter() {
     private var isPlaying: Boolean = false
+    private var skip: Boolean = false
+    private lateinit var binding: RandomSoundsListViewItemBinding
+
     override fun getItem(position: Int): Any {
         return this.arraySoundsItems[position]
     }
@@ -28,45 +30,46 @@ class SoundsListAdapter(var context: Context, var arraySoundsItems: ArrayList<So
 
     @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view:View = View.inflate(this.context, R.layout.random_sounds_list_view_item,null)
-        val deleteButton: ImageView = view.findViewById(R.id.removeButtonImage)
-        val playButton: ImageView = view.findViewById(R.id.playButtonImage)
-        val soundName: TextView = view.findViewById(R.id.soundName)
-        val progressBar: ProgressBar = view.findViewById(R.id.progressBarList)
-        val player = SoundManager(this.context)
-        soundName.text = this.arraySoundsItems[position].nameOfSound
+        binding = RandomSoundsListViewItemBinding.inflate(LayoutInflater.from(parent?.context), parent, false)
+        binding.soundName.text = this.arraySoundsItems[position].nameOfSound
 
-        deleteButton.setOnClickListener {
-            player.removeFile(this.arraySoundsItems[position].filename)
-            this.arraySoundsItems.removeAt(position)
-            notifyDataSetChanged()
-        }
-
-        playButton.setOnClickListener{
-            if(!isPlaying) {
-                isPlaying = true
-                playButton.setImageResource(R.drawable.ic_skip)
-                var currentPosition: Int
-                player.playSound(this.arraySoundsItems[position].filename)
-                Thread{
-                    val duration = player.getDuration()
-                    progressBar.max = duration
-                    while(player.getCurrentPosition() < duration) {
-                        currentPosition = player.getCurrentPosition()
-                        progressBar.progress = currentPosition
-                    }
-                    playButton.setImageResource(R.drawable.ic_play)
-                    progressBar.progress = 0
-                }.start()
-            }
-            else{
-                playButton.setImageResource(R.drawable.ic_play)
-                player.skipSound()
-                isPlaying = false
-            }
-        }
-        return view
+        binding.removeButtonImage.setOnClickListener { deleteButtonClicked(position) }
+        binding.playButtonImage.setOnClickListener { playButtonClicked(position) }
+        return binding.root
     }
 
+    private fun playButtonClicked(position: Int){
+        val manager = SoundManager(this.context)
+        if(!isPlaying) {
+            isPlaying = true
+            binding.playButtonImage.setImageResource(R.drawable.ic_skip)
+            Thread{
+                var currentPosition: Int
+                manager.playSound(this.arraySoundsItems[position].filename)
+                val duration = manager.getDuration()
+                binding.progressBarList.max = duration
+                while(manager.getCurrentPosition() < duration && !skip) {
+                    currentPosition = manager.getCurrentPosition()
+                    binding.progressBarList.progress = currentPosition
+                }
+                manager.skipSound()
+                skip = false
+                isPlaying = false
+                binding.playButtonImage.setImageResource(R.drawable.ic_play)
+                binding.progressBarList.progress = 0
+            }.start()
+        }
+        else{
+            binding.playButtonImage.setImageResource(R.drawable.ic_play)
+            skip = true
+            isPlaying = false
+        }
+    }
 
+    private fun deleteButtonClicked(position: Int) {
+        val player = SoundManager(this.context)
+        player.removeFile(this.arraySoundsItems[position].filename)
+        this.arraySoundsItems.removeAt(position)
+        notifyDataSetChanged()
+    }
 }
